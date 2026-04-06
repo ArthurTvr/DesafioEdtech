d31b62d4842bc953c496130c632d3de4da4262d5
unknown <arthurdanilotavares@hotmail.com>
2026-04-06 09:53:52 -0300

    Fix swiper

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".mySwiper", {
-    loop: true,
+const swiper = new Swiper(".mySwiper", {
+  loop: false,
 
-    navigation: {
-      nextEl: ".slider-controls .swiper-button-next",
-      prevEl: ".slider-controls .swiper-button-prev",
-    },
+  navigation: {
+    nextEl: ".slider-controls .swiper-button-next",
+    prevEl: ".slider-controls .swiper-button-prev",
+  },
 
-    pagination: {
-      el: ".slider-controls .swiper-pagination",
-      clickable: true,
-    },
-  });
-});
-
-const cards = document.querySelectorAll(".card");
-
-cards.forEach((card) => {
-  const buttons = card.querySelectorAll(".btn");
-
-  buttons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const isActive = card.classList.contains("active");
-
-      // fecha todos
-      cards.forEach(c => c.classList.remove("active"));
-
-      // se não estava ativo, abre
-      if (!isActive) {
-        card.classList.add("active");
-      }
-    });
-  });
+  pagination: {
+    el: ".slider-controls .swiper-pagination",
+    clickable: true,
+  },
 });
 // Audio
 
